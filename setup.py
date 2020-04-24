6370fbda657e0d0cd95057afc94e45fdf170e4c5
Aaron Mangum <aaron.mangum@juvo.com>
2020-04-24 16:15:51 -0700

    updated psycopg2 package

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import find_packages, setup
 
 setup(
     name='street_cred',
+    version='0.0.1',
+    author="Aaron Mangum",
     packages=find_packages(),
     install_requires=[
         'cryptography~=2.9',
-        'psycopg2~=2.8.5',
+        'psycopg2-binary~=2.8.5',
         'sqlalchemy~=1.3.15',
     ]
 )
