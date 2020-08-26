31221e782b9d75dd739989214df45636f6f49fa9
Aaron Mangum <aaron.mangum@juvo.com>
2020-08-26 09:25:17 -0700

    update version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import find_packages, setup
 
 setup(
     name='street_cred',
-    version='0.0.2',
+    version='0.0.3',
     author="Aaron Mangum",
     packages=find_packages(),
     install_requires=[
         'cryptography~=2.9',
         'psycopg2-binary~=2.8.5',
         'sqlalchemy~=1.3.15',
-    ]
+    ],
 )
